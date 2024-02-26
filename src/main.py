4892daa3026935afb0b0da732c96704391456449
Tanay Alpkonur <107621923+taannaayyy@users.noreply.github.com>
2024-02-26 19:14:45 +0300

    added placing symbols code

--- src/main.py
+++ src/main.py
@@ -27,6 +27,33 @@ def rgba2rgb(rgba, background=(255, 255, 255)):
     return np.asarray(rgb, dtype='uint8')
 
 
+# Basic class to handle the timestamp of the different sensors to know if it is a new sensors_data or an old one
+class TimestampHandler:
+    def __init__(self):
+        self.t_imu = sl.Timestamp()
+        self.t_baro = sl.Timestamp()
+        self.t_mag = sl.Timestamp()
+
+    ##
+    # check if the new timestamp is higher than the reference one, and if yes, save the current as reference
+    def is_new(self, sensor):
+        if isinstance(sensor, sl.IMUData):
+            new_ = (sensor.timestamp.get_microseconds() > self.t_imu.get_microseconds())
+            if new_:
+                self.t_imu = sensor.timestamp
+            return new_
+        elif isinstance(sensor, sl.MagnetometerData):
+            new_ = (sensor.timestamp.get_microseconds() > self.t_mag.get_microseconds())
+            if new_:
+                self.t_mag = sensor.timestamp
+            return new_
+        elif isinstance(sensor, sl.BarometerData):
+            new_ = (sensor.timestamp.get_microseconds() > self.t_baro.get_microseconds())
+            if new_:
+                self.t_baro = sensor.timestamp
+            return new_
+
+
 def main():
     # Create a Camera object
     zed = sl.Camera()
@@ -47,10 +74,27 @@ def main():
     image = sl.Mat()
     runtime_parameters = sl.RuntimeParameters()
     images_all = np.array([])
+    magnetometerdata = []
 
-    while i < 150:
+    while i < 10:
         # Grab an image, a RuntimeParameters object must be given to grab()
         if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+
+            # Used to store the sensors timestamp to know if the sensors_data is a new one or not
+            ts_handler = TimestampHandler()
+
+            # Get Sensor Data for 5 seconds
+            sensors_data = sl.SensorsData()
+            if zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT) == sl.ERROR_CODE.SUCCESS:
+                # Check if the data has been updated since the last time
+                # IMU is the sensor with the highest rate
+                if ts_handler.is_new(sensors_data.get_imu_data()):
+
+                    # Check if Magnetometer data has been updated (not the same frequency than IMU)
+                    if ts_handler.is_new(sensors_data.get_magnetometer_data()):
+                        magnetic_heading = sensors_data.get_magnetometer_data().magnetic_heading
+                        magnetometerdata.append(magnetic_heading)
+
             # A new image is available if grab() returns SUCCESS
             print(i)
             zed.retrieve_image(image, sl.VIEW.LEFT)
@@ -63,7 +107,7 @@ def main():
             print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(image.get_width(), image.get_height(),
                                                                                  timestamp.get_milliseconds()))
             i = i + 1
-
+            print(magnetometerdata)
     # Close the camera
     zed.close()
 
@@ -85,68 +129,5 @@ def stitch():
         cv2.imshow("Stitched Image", stitched_img)
 
 
-# Basic class to handle the timestamp of the different sensors to know if it is a new sensors_data or an old one
-class TimestampHandler:
-    def __init__(self):
-        self.t_imu = sl.Timestamp()
-        self.t_baro = sl.Timestamp()
-        self.t_mag = sl.Timestamp()
-
-    ##
-    # check if the new timestamp is higher than the reference one, and if yes, save the current as reference
-    def is_new(self, sensor):
-        if (isinstance(sensor, sl.IMUData)):
-            new_ = (sensor.timestamp.get_microseconds() > self.t_imu.get_microseconds())
-            if new_:
-                self.t_imu = sensor.timestamp
-            return new_
-        elif (isinstance(sensor, sl.MagnetometerData)):
-            new_ = (sensor.timestamp.get_microseconds() > self.t_mag.get_microseconds())
-            if new_:
-                self.t_mag = sensor.timestamp
-            return new_
-        elif (isinstance(sensor, sl.BarometerData)):
-            new_ = (sensor.timestamp.get_microseconds() > self.t_baro.get_microseconds())
-            if new_:
-                self.t_baro = sensor.timestamp
-            return new_
-
-def magnetometer():
-    # Create a Camera object
-    zed = sl.Camera()
-
-    init_params = sl.InitParameters()
-    init_params.depth_mode = sl.DEPTH_MODE.NONE
-
-    # Open the camera
-    err = zed.open(init_params)
-    if err != sl.ERROR_CODE.SUCCESS:
-        print(repr(err))
-        zed.close()
-        exit(1)
-
-    # Used to store the sensors timestamp to know if the sensors_data is a new one or not
-    ts_handler = TimestampHandler()
-
-    # Get Sensor Data for 5 seconds
-    sensors_data = sl.SensorsData()
-    if zed.get_sensors_data(sensors_data, sl.TIME_REFERENCE.CURRENT) == sl.ERROR_CODE.SUCCESS:
-        # Check if the data has been updated since the last time
-        # IMU is the sensor with the highest rate
-        if ts_handler.is_new(sensors_data.get_imu_data()):
-
-            # Check if Magnetometer data has been updated (not the same frequency than IMU)
-            if ts_handler.is_new(sensors_data.get_magnetometer_data()):
-                magnetic_field_calibrated = sensors_data.get_magnetometer_data().get_magnetic_field_calibrated()
-                print(
-                    " - Magnetometer\n \t Magnetic Field: [ {0} {1} {2} ] [uT]".format(magnetic_field_calibrated[0],
-                                                                                       magnetic_field_calibrated[1],
-                                                                                       magnetic_field_calibrated[
-                                                                                           2]))
-    zed.close()
-    return 0
-
-
 if __name__ == "__main__":
-    # stitch()
-    magnetometer()
+    main()
---
+++ src/placeNtest.py
@@ -0,0 +1,40 @@
+
+
+import cv2
+
+
+def placeDirection(image):
+    # Define the coordinates where you want to place the symbol
+    x, y = 760, 50
+
+    # Define the symbol text (N or S)
+    symbol = "N"
+
+    # Define the font settings
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 4
+    font_thickness = 7
+    font_color = (0, 255, 0)  # White color
+
+    # Get the size of the symbol text
+    text_size = cv2.getTextSize(symbol, font, font_scale, font_thickness)[0]
+
+    # Calculate the position to place the text so that it's centered at (x, y)
+    text_x = x - text_size[0] // 2
+    text_y = y + text_size[1] // 2
+
+    # Place the symbol on the image
+    cv2.putText(image, symbol, (text_x, text_y), font, font_scale, font_color, font_thickness)
+
+
+def main():
+    image = cv2.imread('C:/Users/taalp/Documents/GitHub/Panaroma_image_stitching/unstitchedImages/1.jpeg')
+    placeDirection(image)
+    # Display the image
+    cv2.imshow('Image with symbol', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
